67085da0e8335e34faf5a084df26b9343f0ed848
Anna Onyshchenko <auonishchenko.com>
2025-04-12 16:36:13 +0300

    add task solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,25 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+
+  let partnerInfo;
+
+  if (partnerName && isMarried) {
+    partnerInfo =
+      // eslint-disable-next-line no-nested-ternary
+      sex === 'f'
+        ? `My husband is ${partnerName}`
+        : sex === 'm'
+          ? `My wife is ${partnerName}`
+          : null;
+  } else {
+    partnerInfo = 'I am not married';
+  }
+
+  return (
+    <>
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
+      <p className="Person__age">{age ? `I am ${age}` : null}</p>
+      <p className="Person__partner">{partnerInfo}</p>
+    </>
+  );
+};
